4c78dd0bd7e805cce63f7483bcb355471aeff528
davMac606 <worksender053@gmail.com>
2024-06-04 20:38:48 -0300

    It might fuck up everything I've done and didn't commit, so this is a new branch.

--- main.py
+++ main.py
@@ -1,49 +1,50 @@
-'''def leituraf1():
-    f1 = open("aliquotas2024.txt", "r")
+import tabulate
+
+def main(aliquotas, contribuintes, output):
+    f1 = open(aliquotas+"2024.txt", "r")
+    f2 = open(contribuintes+"2024.txt", "r")
+    
+    f1_lines = f1.readlines()
+    f2_lines = f2.readlines()
+    faixas = []
+    nomes = []
+    
+    def minF():
+        for i in range(len(f1_lines)):
+            f1_lines[i] = f1_lines[i]
+            if i%2== 0:
+                faixas.append(str(f1_lines[i]).strip())
+        return faixas
+    
+    def maxF():
+        for i in range(len(f1_lines)):
+            f1_lines[i] = f1_lines[i]
+            if i%3 == 0:
+                faixas.append(str(f1_lines[i]).strip())
+        return faixas
+    
+    def nome():
+        for i in range(len(f2_lines)):
+            f2_lines[i] = f2_lines[i]  
+            if i%2 == 0:
+                nomes.append(str(f2_lines[i]).strip())
+        return nomes
+    
+    ebatabela = []
+    for i in range(len(maxF())):
+        ebatabela.append([maxF()[i]])
+    '''for i in range(len(nome())):
+        ebatabela.append([nome()[i]])
+    table = tabulate.tabulate(ebatabela, headers=["Nomes", "Impostos"], tablefmt="grid")'''
+    table = tabulate.tabulate(ebatabela, headers=["Minimo","Maximo"], tablefmt="grid")
+    print(table)
     
     
-    lines = f1.readlines()
-    num = 0
-    for i in range(len(lines)):
-        
-        num+=1
-        if (num%4 == 0):
-            num = 0'''
-def leituraf1():
-    f1 = open("aliquotas2024.txt", "r")
-    linesa = f1.readlines()
-
-def leituraf2():
-    f2 = open("contribuintes2024.txt", "r")
-    linesc = f2.readlines()
-                        
-'''def faixa1(minSal,maxSal,salary,aliquota):
-    if (salary >= minSal and salary <= maxSal):
-        aliq = aliquota
-    return aliq
-
-def faixa2(minSal,maxSal,salary,aliquota):
-    if (salary >= minSal and salary <= maxSal):
-        aliq = aliquota
-    return aliq
-
-def faixa3(minSal,maxSal,salary,aliquota):
-    if (salary >= minSal and salary <= maxSal):
-        aliq = aliquota
-    return aliq
-
-def faixa4(minSal,maxSal,salary,aliquota):
-    if (salary >= minSal and salary <= maxSal):
-        aliq = aliquota
-    return aliq
-    
-def faixa5(minSal,maxSal,salary,aliquota):
-    if (salary >= minSal and salary <= maxSal):
-        aliq = aliquota
-    return aliq'''
-
-if __name__ == "main":
+if __name__ == "__main__":
     print("Seja bem vindo ao programa de cálculo de imposto de renda!")
-    nome = input("Digite o nome a ser conferido: ")
+    output = input("Digite o nome do arquivo de saída: ")
+    main("aliquotas", "contribuintes", output)
+
+    
 
 
